a9b3758f993c1b5054296cab9871d52d4d0fc377
Priya Kumari <priya.k.p.13579@gmail.com>
2025-04-07 01:06:36 +0530

    done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 
+
 function App() {
+const img = "https://picsum.photos/200";
+  
   return (
-    <div className="App">
-      
+    <div>
+     
+     <h1 className='app'>Random Images Generator</h1>
+      <img className='img' src={img}/>
+     
      
     </div>
   );
